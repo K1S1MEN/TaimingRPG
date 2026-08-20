4e2255ff8512d7422c3c31e055a4af40cece082a
AIR\vantan <suzuho9@gmail.com>
2026-08-20 09:36:35 +0900

    エンカウントの根本部分完成

--- Assets/Script/playerMove.cs
+++ Assets/Script/playerMove.cs
@@ -20,5 +20,13 @@ public class playerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         transform.Translate(x * Time.deltaTime*speed, y * Time.deltaTime*speed, 0);
+        if (x != 0 || y != 0)
+        {
+            if (Random.Range(0, 150) == 0)
+            {
+                Debug.Log("エンカウント！！");
+            }
+
+        }
     }
 }
